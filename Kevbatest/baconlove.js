4e276ecff490056ab1f3184a101ba8f0700fc67a
JsFina <josefina.gabrielsson@gmail.com>
2016-05-21 14:56:40 +0200

    kod öfr fgafn

---
+++ Kevbatest/baconlove.js
@@ -0,0 +1,34 @@
+﻿var list = {
+    movie1: {
+        Title: "Elf",
+        MovieImage: "http://ia.media-imdb.com/images/M/MV5BNjY1NjQ3NDY5MF5BMl5BanBnXkFtZTYwODAyMTc3._V1_UX182_CR0,0,182,268_AL_.jpg",
+        ActorImage: "http://www.ultimatemovierankings.com/wp-content/uploads/2015/08/ferrell-1111.jpg",
+        Actor: "Will Ferrell",
+    },
+    movie2: {
+        Title: "Ghostbusters",
+        MovieImage: "http://vignette2.wikia.nocookie.net/ghostbusters/images/0/01/Ghostbusters_ii_poster.jpg/revision/latest/scale-to-width-down/270?cb=20100531182552",
+        ActorImage: "http://www.nndb.com/people/537/000024465/murray-mmnet_c0d7b094518e.jpg",
+        Actor: "Bill Murray"
+    },
+    movie3: {
+        Title: "Pulp fiction",
+        MovieImage: "http://ia.media-imdb.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+        ActorImage: "http://ia.media-imdb.com/images/M/MV5BMjMxNzk1MTQyMl5BMl5BanBnXkFtZTgwMDIzMDEyMTE@._V1_UX214_CR0,0,214,317_AL_.jpg",
+        Actor: "Uma Thurman"
+    },
+    movie4: {
+        Title: "Yes man",
+        MovieImage: "http://ia.media-imdb.com/images/M/MV5BNjYyOTkyMzg2OV5BMl5BanBnXkFtZTcwODAxNjk3MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+        ActorImage: "http://ia.media-imdb.com/images/M/MV5BMTQwMjAwNzI0M15BMl5BanBnXkFtZTcwOTY1MTMyOQ@@._V1_UY317_CR22,0,214,317_AL_.jpg",
+        Actor: "Jim Carrey"
+    },
+    movie5: {
+        Title: "Forest Gump",
+        MovieImage: "http://ia.media-imdb.com/images/M/MV5BMTI1Nzk1MzQwMV5BMl5BanBnXkFtZTYwODkxOTA5._V1_UY268_CR2,0,182,268_AL_.jpg",
+        ActorImage: "http://ia.media-imdb.com/images/M/MV5BMTQ2MjMwNDA3Nl5BMl5BanBnXkFtZTcwMTA2NDY3NQ@@._V1_UY317_CR2,0,214,317_AL_.jpg",
+        Actor: "Tom Hanks"
+    }
+}
+
+console.log(list);
